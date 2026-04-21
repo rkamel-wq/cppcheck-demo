d3d922aca8f250c46f46ec7c79d8aa811053519d
Rola Kamel <Rkamel@aucegypt.edu>
2026-04-21 13:03:27 +0000

    identifying errors using cppchech

--- main.cpp
+++ main.cpp
@@ -31,28 +31,28 @@ class DataManager {
     return data[0];
   }
 
-  ~DataManager() { delete data; }
+  ~DataManager() { delete[] data; }
 };
 
 void unsafeFunction() {
-  char buffer[10];
+  char* buffer= new char[100];
 
   strcpy(buffer, "This is way too long for buffer");
 
   cout << buffer << endl;
+	delete[] buffer;
 }
 
 int globalVar = 0;
 
 int compute(int x) {
-  int result;
 
   if (x > 10)
-    result = x * 2;
+    return x * 2;
   else if (x < 0)
-    return result;
+    return 0;
+	return x;
 
-  return result;
 }
 
 void memoryLeakDemo() {
@@ -63,6 +63,8 @@ void memoryLeakDemo() {
   *leak2 = 20;
 
   cout << leak1[0] + *leak2 << endl;
+	 delete[] leak1;
+        delete leak2;
 }
 
 void vectorIssues() {
@@ -71,10 +73,11 @@ void vectorIssues() {
   v.push_back(1);
   v.push_back(2);
 
-  cout << v[10] << endl;
+  cout << v[1] << endl;
 
-  v.clear();
   cout << v.front() << endl;
+	 v.clear();
+
 }
 
 int main() {
